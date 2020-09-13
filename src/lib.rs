73562d85227f3e0fd2fdd5a607a82a69daa2b3f2
jtr109 <conbas2019@gmail.com>
2020-09-13 20:14:05 +0800

    feat: add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//! A convertor between [`usize`] and [`String`] in [Base62].
+//!
+//! # Examples
+//!
+//! ```rust
+//! use base62num::{encode, decode, Base62Error};
+//!
+//! // It can convert an usize number into a string in Base62.
+//! assert_eq!(encode(123), "B9");
+//!
+//! // Vice versa.
+//! assert_eq!(decode("B9").unwrap(), 123);
+//!
+//! // Any non-alphanumeric character in the string is invalid.
+//! assert_eq!(decode("Base*62"), Err(Base62Error::NonAlphanumeric));
+//!
+//! // The conversion also failed if the result number is overflow.
+//! assert_eq!(
+//!     decode("AStringTooLongCausesTheOverflowError"),
+//!     Err(Base62Error::Overflow)
+//! );
+//! ```
+//! [`usize`]: https://doc.rust-lang.org/std/primitive.usize.html
+//! [`String`]: https://doc.rust-lang.org/std/string/struct.String.html
+//! [Base62]: https://en.wikipedia.org/wiki/Base62
+
 use std::char;
 use std::error::Error;
 use std::fmt;
@@ -5,9 +31,31 @@ use std::fmt;
 const ALPHANUMERIC: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 const BASE: usize = 62;
 
+/// Conversion errors between numbers and strings in Base62.
 #[derive(Debug, PartialEq)]
 pub enum Base62Error {
+    /// Occurs when decoding a string contains non-alphanumeric characters.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use base62num::{decode, Base62Error};
+    ///
+    /// assert_eq!(decode("Base*62"), Err(Base62Error::NonAlphanumeric));
+    /// ```
     NonAlphanumeric,
+    /// Occurs when the result of decoding is overflow.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use base62num::{decode, Base62Error};
+    ///
+    /// assert_eq!(
+    ///     decode("AStringTooLongCausesTheOverflowError"),
+    ///     Err(Base62Error::Overflow)
+    /// );
+    /// ```
     Overflow,
 }
 
@@ -33,6 +81,18 @@ fn to_char(num: usize) -> Option<char> {
     ALPHANUMERIC.chars().nth(num)
 }
 
+/// Converts a number into a string in Base62.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```rust
+/// use base62num::encode;
+///
+/// assert_eq!(encode(7), "H");
+/// assert_eq!(encode(123), "B9");
+/// ```
 pub fn encode(num: usize) -> String {
     let mut digits = vec![];
     let mut n = num;
@@ -53,6 +113,22 @@ fn to_num(c: char) -> Result<usize, Base62Error> {
         .ok_or(Base62Error::NonAlphanumeric)
 }
 
+/// Converts a string in Base62 into an number.
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```rust
+/// use base62num::{decode, Base62Error};
+///
+/// assert_eq!(decode("H").unwrap(), 7);
+/// assert_eq!(decode("B9").unwrap(), 123);
+/// assert_eq!(decode("Base*62"), Err(Base62Error::NonAlphanumeric));
+/// assert_eq!(
+///     decode("AStringTooLongCausesTheOverflowError"),
+///     Err(Base62Error::Overflow)
+/// );
+///
 pub fn decode(input: &str) -> Result<usize, Base62Error> {
     input.chars().try_fold(0 as usize, |acc, c| {
         to_num(c).and_then(|x| {
